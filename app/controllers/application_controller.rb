dcced1a29dade8ea995cfd6bd5974e3f235f8bb7
Zach Nusbaum <zach@zdnmail.com>
2018-09-19 12:05:33 -0700

    Make the admin panel accessible to admins only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def authenticate_admin_user!
+    unless user_signed_in? && current_user.admin
+      redirect_to root_url, notice: 'Sorry, you are not an admin.'
+    end
+  end
 end
