ab955f535e47a29136cd3ca9edc97209f9a25b78
Zach Nusbaum <zach@zdnmail.com>
2018-10-02 09:15:38 -0700

    Add footer; change store name

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -2,6 +2,36 @@ ActiveAdmin.register Product do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :description, :category_id, :price_cents, :quantity, :weight, :published
+permit_params :name, :description, :category_id, :price_cents, :quantity, :weight, :published, images: []
+
+  form do |f|
+    f.inputs 'Product Details' do
+      f.input :category
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :quantity
+      f.input :weight
+      f.input :published
+      f.input :images, as: :file, input_html: { multiple: true }
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :category
+      row :name
+      row :description
+      row :price
+      row :quantity
+      row :weight
+      row :published
+    end
+
+    product.images.each do |image|
+      para raw("<img src=\"#{url_for image}\">")
+    end
+  end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   has_many :cart_items
   has_many :orders, through: :cart_items
   belongs_to :category
+  has_many_attached :images
   monetize :price_cents
 end
