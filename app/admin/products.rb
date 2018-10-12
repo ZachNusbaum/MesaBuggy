26cb68726ce5b22b21c3885aa0421e40f32c9a96
Zach Nusbaum <zach@zdnmail.com>
2018-10-11 21:44:17 -0700

    can edit price in admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Product do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :description, :category_id, :price_cents, :quantity, :weight, :published, images: []
+permit_params :name, :description, :category_id, :price_cents, :price, :quantity, :weight, :published, images: []
 
   form do |f|
     f.inputs 'Product Details' do
