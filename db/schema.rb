d02f01d8cec96a07bedd04d51490c88aae06f665
Zach Nusbaum <zach@zdnmail.com>
2018-10-24 22:11:35 -0700

    Add 'Review' model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_034802) do
+ActiveRecord::Schema.define(version: 2018_10_25_051023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -257,6 +257,18 @@ ActiveRecord::Schema.define(version: 2018_10_25_034802) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.integer "rating", default: 1, null: false
+    t.text "body"
+    t.boolean "hidden", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -275,4 +287,6 @@ ActiveRecord::Schema.define(version: 2018_10_25_034802) do
   add_foreign_key "order_coupons", "coupons"
   add_foreign_key "order_coupons", "orders"
   add_foreign_key "products", "categories"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
