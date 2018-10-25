c79b827ab816a7fdd49f3e4cb0a4efc30356869f
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 14:48:37 -0700

    Add first and last name to addresses

---
+++ db/migrate/20181025214825_add_first_and_last_name_to_addresses.rb
@@ -0,0 +1,6 @@
+class AddFirstAndLastNameToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :addresses, :first_name, :string
+    add_column :addresses, :last_name, :string
+  end
+end
