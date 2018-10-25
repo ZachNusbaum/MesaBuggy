cebc99235587b1841bd98290ebbbeec173dc6e7b
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 16:40:39 -0700

    allow use of zip code api without authentication

--- app/controllers/api/zip_codes_controller.rb
+++ app/controllers/api/zip_codes_controller.rb
@@ -1,10 +1,12 @@
 class Api::ZipCodesController < ApplicationController
+  skip_before_action :authenticate_user!
   def lookup
-    @zip = Postjoy.find(params[:code])
+    @zip = Postjoy.find(params[:code].split('-')[0])
     if @zip
-      render json: {success: true, result: @zip}
+      render json: {success: true, input: params[:code], result: @zip}
     else
-      render json: {success: false, result: nil}
+      render json: {success: false, input: params[:code], result: nil}
     end
+    response.set_header('X-Mesa-Buggy-API-UID', SecureRandom.uuid)
   end
 end
