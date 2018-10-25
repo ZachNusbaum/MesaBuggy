643e8c4399443851496ae49368b9a51e4ce435aa
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 15:06:37 -0700

    Add first and last name to user registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_search_query
   before_action :track_page_visit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def authenticate_admin_user!
     unless user_signed_in? && current_user.admin
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
 
   def track_page_visit
     ahoy.track "Access Route", request.path_parameters
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
