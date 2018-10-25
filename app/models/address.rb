ace317cc352ff035968a3eacc860549252febe5e
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 16:06:20 -0700

    Add zip code parsing

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,20 @@
 class Address < ApplicationRecord
   belongs_to :user
   has_secure_token
+
+  validates :zip_code, postal_code: true
+
+  before_save :parse_zip_code
+
+  private
+
+  def parse_zip_code
+    if zip_code_changed?
+      parsed_zip = Postjoy.find(zip_code)
+      if parsed_zip
+        self.latitude = parsed_zip[:latitude]
+        self.longitude = parsed_zip[:longitude]
+      end
+    end
+  end
 end
