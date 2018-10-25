19c94e01ade331d00318d47a258c38266950ac70
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 15:21:53 -0700

    Change menu for signed in users; add link to account edit page

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :email, :password, :password_confirmation, :admin
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation, :admin
 
   index do
     selectable_column
@@ -14,6 +14,8 @@ ActiveAdmin.register User do
   form do |f|
     f.semantic_errors
     f.inputs 'User Details' do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
