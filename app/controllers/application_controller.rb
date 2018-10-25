2ce1a874da5ae4b3775d1e110a5386902c842eb0
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 15:43:47 -0700

    set raven context for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_search_query
   before_action :track_page_visit
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_raven_context
 
   def authenticate_admin_user!
     unless user_signed_in? && current_user.admin
@@ -23,5 +24,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def set_raven_context
+    Raven.user_context(id: session[:current_user_id]) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
