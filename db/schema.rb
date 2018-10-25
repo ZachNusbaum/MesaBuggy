6dfeec504da431644b8b9afc6e52df4fda02554e
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 14:49:09 -0700

    Run migration: AddFirstAndLastNameToAddresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_214708) do
+ActiveRecord::Schema.define(version: 2018_10_25_214825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2018_10_25_214708) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["token"], name: "index_addresses_on_token", unique: true
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
