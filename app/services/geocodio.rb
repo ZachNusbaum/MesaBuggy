c0b72e08e1901a01898b821f0778d4e1a67e82fc
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 20:41:14 -0700

    request only timezone as additional geocode data, to reduce api key usage

--- app/services/geocodio.rb
+++ app/services/geocodio.rb
@@ -2,6 +2,6 @@ class Geocodio
   include HTTParty
   base_uri 'api.geocod.io/v1.3'
   def self.geocode(query)
-    get("/geocode?q=#{query}&api_key=#{ENV['GEOCODIO_API_KEY']}&fields=cd,stateleg,school,census,timezone")
+    get("/geocode?q=#{query}&api_key=#{ENV['GEOCODIO_API_KEY']}&fields=timezone")
   end
 end
