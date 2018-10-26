ab59fb91e1f85b240f9f9e31bc7851857a26c7b2
Zach Nusbaum <zach@zdnmail.com>
2018-10-25 20:35:54 -0700

    add additional fields to geocode

--- app/services/geocodio.rb
+++ app/services/geocodio.rb
@@ -2,6 +2,6 @@ class Geocodio
   include HTTParty
   base_uri 'api.geocod.io/v1.3'
   def self.geocode(query)
-    get("/geocode?q=#{query}&api_key=#{ENV['GEOCODIO_API_KEY']}")
+    get("/geocode?q=#{query}&api_key=#{ENV['GEOCODIO_API_KEY']}&fields=cd,stateleg,school,census,timezone")
   end
 end
